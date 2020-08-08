b31c42bf5a26e9e39360c437302141081c579c81
Brooke Nemchak <bnemchak@gmail.com>
2020-08-08 13:27:39 -0400

    setup

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Sports Roster</h2>
+        <button className="btn btn-secondary">Team</button>
+      </div>
+    );
+  }
+}
+
+export default App;
